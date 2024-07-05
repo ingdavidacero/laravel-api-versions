83c0bfd80df9b131d6ec9d740a013771926efa07
David Acero <ing.david.acero@gmail.com>
2024-07-04 19:38:05 -0500

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# API WITH LARAVEL 
+Project to create an API using Laravel with versioning.
+
+laravel version: "11.9"
+sanctum version: 4.0
---
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Models\Post;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Resources\V1\PostResource;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return response()->json([
+            'status' => 'OK',
+            'data' => PostResource::collection(Post::latest()->paginate())
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        return response()->json([
+            'status' => 'OK',
+            'data' => new PostResource($post)
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+
+        return response()->json([
+            'status' => 'OK',
+        ],204);
+    }
+}
